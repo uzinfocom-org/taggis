65126df6efe6f5d90237b8ceaadb15ec2b07883c
Yūri Katsuki <yuri@katsuki.moe>
2023-03-15 15:27:48 +0500

    [FIX] command inliner

--- components/icons/command.js
+++ components/icons/command.js
@@ -1,10 +1,19 @@
 const Command = () => {
   return (
-    <svg height="30" viewBox="0 0 24 24" fill="none" stroke="var(--fg)" strokeWidth="2">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h7" />
+    <svg
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="var(--fg)"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4 6h16M4 12h16M4 18h7"
+      />
     </svg>
   )
 }
 
 export default Command
-
