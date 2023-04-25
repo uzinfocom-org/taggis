b165c9ac84fbb14d1a787e3ca249702e8c520e85
Yūri Katsuki <yuri@katsuki.moe>
2023-04-25 17:08:07 +0500

    [FIX] adding json dynamic url resolver

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-import * as social from "./social.config.mjs"
+import socials from "./socials.json" assert { type: "json" }
 
 const nextConfig = {
   swcMinify: true,
   async redirects() {
-    return social.links
+    return socials.data.map((social) => ({
+      source: `/${social.title.toLowerCase()}`,
+      destination: social.url,
+      permanent: true,
+    }))
   },
   output: 'standalone',
 }
