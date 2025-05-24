7b3342dc2454fa9f6ceff07591ec2eab18e1fe70
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-24 22:37:43 +0500

    chore: defined base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 import socials from './socials.json' with { type: 'json' }
 
+const BASE_PATH = process.env.BASE_PATH;
+
 const nextConfig = {
   output: "standalone",
   swcMinify: true,
+  basePath: BASE_PATH || "",
   async redirects() {
     return socials.data.map((social) => ({
       source: `/${social.title.toLowerCase()}`,
